ad80f7cdb6ec9a6fa7cf0c4343016d01d6d47a16
AndyS1mpson <werter1555rus@mail.ru>
2020-03-07 19:30:15 +0300

    fix IsPointInFieldAlgorithm

--- DozeField/DozeField.cs
+++ DozeField/DozeField.cs
@@ -2,14 +2,29 @@ using System;
 
 namespace DozeField
 {
+    public struct Line
+    {
+        // ax+by+c=0;
+        public double a;
+        public double b;
+        public double c;
+        public Line(double _a,double _b,double _c)
+        {
+            var normCoef = (_c > 0 ? -1 : 1) / Math.Sqrt(_a * _a + _b * _b);
+
+            a = _a * normCoef;
+            b = _b * normCoef;
+            c = _c * normCoef;
+        }
+        
+    }
     public class DozeField
     {
-        private (double,double)[] _field;   //polygon
+        private (double,double)[] _field;
 
         public DozeField((double,double)[] field)
-        {
-            _field=field;
-        }
+            =>_field=field;
+        
         //metric measurement function
         public double Metric((double,double) point1,(double,double) point2)
         {
@@ -17,72 +32,104 @@ namespace DozeField
                                         +(point1.Item2-point2.Item2)*(point1.Item2-point2.Item2));
             return metric;
         }
-
-        //Find the equiption coefficients 2 perpendicular straights
-        public (double,double,double[,])  FindingIntersectionOfPerpendicularStraights((double,double) point1,(double,double) point2,(double,double) point0)
+        
+        //find the coefficients of the equation of the line passing through 2 points
+        public Line FindCoefficientsOfEquation((double,double) point1, (double,double) point2)
         {
-            double[,] equationCoefficients=new double[2,3];
-            //ax+by+c=0 -current side of field
-            equationCoefficients[0,0]=point2.Item2-point1.Item2;                    //a
-            equationCoefficients[0,1]=-(point2.Item1-point1.Item1);                 //b
-            equationCoefficients[0,2]=-equationCoefficients[0,1]*point1.Item2       //c
-                                        -equationCoefficients[0,0]*point1.Item1;
-            //a1x+b1y+c1=0 - coefficients straight perpendicular to the side                             
-            equationCoefficients[1,0]=equationCoefficients[0,1];                    //a1
-            equationCoefficients[1,1]=-equationCoefficients[0,0];                   //b1
-            equationCoefficients[1,2]=equationCoefficients[0,0]*point0.Item2        //c1
-                                         - equationCoefficients[0,1]*point0.Item1;
-
-            (double,double,double[,]) intersectionOfLines;
-            intersectionOfLines.Item2=(equationCoefficients[0,0]*equationCoefficients[1,2]
-                                            -equationCoefficients[0,1]*equationCoefficients[0,2])
-                                            /(equationCoefficients[0,1]*equationCoefficients[0,1]
-                                            +equationCoefficients[0,0]*equationCoefficients[0,0]);
-            intersectionOfLines.Item1=(-equationCoefficients[0,1]*intersectionOfLines.Item2
-                                            -equationCoefficients[0,2])/equationCoefficients[0,0];
-            double[,] normalCoordinates=new double[2,2];
-            normalCoordinates[0,0]=-equationCoefficients[0,0];
-            normalCoordinates[0,1]=-equationCoefficients[0,1];
-            normalCoordinates[1,0]=intersectionOfLines.Item1-point0.Item1;
-            normalCoordinates[1,1]=intersectionOfLines.Item2-point0.Item2;
-            intersectionOfLines.Item3=normalCoordinates;
-            return intersectionOfLines;
+            double a = point2.Item2-point1.Item2;
+            double b = -(point2.Item1-point1.Item1);
+            double c = -b*point2.Item2           
+                            -a*point2.Item1;
+            Line coefficients=new Line(a,b,c);
+            return coefficients;
         }
 
-        public double CosBetweenVectors(double[,] vectors)
+        //find the coefficients of the equation of the perpendicular line passing current point
+        public Line FindNormalCoefficients(Line lineCoefficients,(double,double) point0)
         {
-            double cos=(vectors[0,0]*vectors[1,0]+vectors[0,1]*vectors[1,1])/Math.Sqrt(vectors[0,0]*vectors[0,0]+vectors[0,1]*vectors[0,1])
-                                        *Math.Sqrt(vectors[1,0]*vectors[1,0]+vectors[1,1]*vectors[1,1]);
+            Line normalCoefficients;
+            normalCoefficients.a = lineCoefficients.b;
+            normalCoefficients.b = -lineCoefficients.a;
+            normalCoefficients.c = lineCoefficients.a*point0.Item2
+                                    - lineCoefficients.b*point0.Item1;
+            return normalCoefficients;
+        }
+    
+        //find intersection of perpendicular lines
+        public (double,double) IntersectionOfPerpendicularLines(Line line,Line normalLine)
+        {
+            (double,double) intersection;
+            intersection.Item2 = (line.a*normalLine.c - line.b*line.c)/(line.a*line.a + line.b*line.b);
+            intersection.Item1 = (-line.b*intersection.Item2 -line.c)/line.a;
+            return intersection;
+        }
+    
+        //cosine of angle between two vectors
+        public double CosOfAngleBetweenVectors((double,double) vector1,(double,double) vector2)
+        {
+            double cos=(vector1.Item1*vector2.Item1 + vector1.Item2*vector2.Item2)
+                                        /Math.Sqrt(vector1.Item1*vector1.Item1 + vector1.Item2*vector1.Item2)
+                                        *Math.Sqrt(vector2.Item1*vector2.Item1 + vector2.Item2*vector2.Item2);
             return cos;
         }
-        //Check the point for belonging to the polygon 
-        public bool PointAffiliationAlgorithm((double,double) point)
+
+        // check point is in field or not
+        public bool IsPointInField((double,double) point)
         {
+            bool result=false;
+            (double,double) normalSideVector=(0,0);             // normal vector of the current side
+            (double,double) guideLineVector=(0,0);              // line direction vector from point to intersection
+            (double,double) intersection;                       // intersection of 2 lines
+            double lengthGuideLineVector=0;                     // vector length
             
-             // Intersection perpendicular straights 
-             (double,double,double[,]) intersection=FindingIntersectionOfPerpendicularStraights(_field[0],_field[1],point);
-            double minLength=Metric(point,(intersection.Item1,intersection.Item2));
-            
-            //Find min distance to the sides
-            for(int i=1;i< _field.Length-1;i++)
+            for(int i=0;i<_field.Length-1;i++)
             {
-                (double,double,double[,]) nextIntersection=FindingIntersectionOfPerpendicularStraights(_field[i],_field[i+1],point);
-                if(minLength>Metric(point,(nextIntersection.Item1,nextIntersection.Item2)))
-                    if(_field[i].Item1<_field[i+1].Item1)
-                        if(nextIntersection.Item1<_field[i+1].Item1 && nextIntersection.Item1>_field[i].Item1)
-                            intersection=nextIntersection;
-                    else if(_field[i].Item1>_field[i+1].Item1)
-                        if(nextIntersection.Item1>_field[i+1].Item1 && nextIntersection.Item1<_field[i].Item1)
-                            intersection=nextIntersection;
+                if(point == _field[i])
+                {       
+                    result=true;
+                    break;
+                }
+                
+                Line checkingSide;                              // equation coefficients of the current side
+                Line checkingLine;                              // equation coefficients of the line perpendicular to the side
+                (double,double) curInter;
+                (double,double) guideCurLineVector;
+                checkingSide = FindCoefficientsOfEquation(_field[i],_field[i+1]);
+                checkingLine = FindNormalCoefficients(checkingSide,point);
+                curInter = IntersectionOfPerpendicularLines(checkingSide,checkingLine);
+                guideCurLineVector = (curInter.Item1 - point.Item1,curInter.Item2 - point.Item2);
+
+                if(lengthGuideLineVector>Metric(curInter,point) || lengthGuideLineVector==0)
+                    if(_field[i].Item1 <= _field[i+1].Item1)
+                        {
+                            if(curInter.Item1 >= _field[i].Item1 && curInter.Item1 <= _field[i+1].Item1)
+                            if((_field[i].Item2 < _field[i+1].Item2 && curInter.Item2 >= _field[i].Item2 && curInter.Item2 <= _field[i+1].Item2)
+                                         || (_field[i].Item2 > _field[i+1].Item2 && curInter.Item2 >= _field[i+1].Item2 && curInter.Item2 <= _field[i].Item2))
+                            {
+                                normalSideVector = (-checkingSide.a,-checkingSide.b);
+                                guideLineVector = guideCurLineVector;
+                                intersection = curInter;
+                                lengthGuideLineVector = Metric(curInter,point);
+                            }
+                        }
+                    else if(_field[i].Item1 > _field[i+1].Item1)
+                        if(curInter.Item1 <= _field[i].Item1 && curInter.Item1 >= _field[i+1].Item1)
+                        if((_field[i].Item2 < _field[i+1].Item2 && curInter.Item2 >= _field[i].Item2 && curInter.Item2 <= _field[i+1].Item2)
+                                         || (_field[i].Item2 > _field[i+1].Item2 && curInter.Item2 >= _field[i+1].Item2 && curInter.Item2 <= _field[i].Item2))
+                        {
+                            normalSideVector = (checkingSide.a,checkingSide.b);
+                            guideLineVector = guideCurLineVector;
+                            intersection = curInter;
+                            lengthGuideLineVector = Metric(curInter,point);
+                        }
+
 
             }
-            bool signCos;        //cosine sign of the angle between normal and guide vectors  
-                                 //+ == true , - == false
-            //if(intersection.Item3[0,0] == -intersection.Item3[1,1])
-            if(CosBetweenVectors(intersection.Item3)>=0)
-                signCos=true;
-            else signCos=false;
-            return signCos;
+            if(CosOfAngleBetweenVectors(normalSideVector,guideLineVector)>=0)
+                result=true;
+                
+        return result;
         }
+    
     }
 }
--- DozeField/Program.cs
+++ DozeField/Program.cs
@@ -7,13 +7,13 @@ namespace DozeField
         static void Main(string[] args)
         {
             (double,double)[] temp=new (double, double)[5];
-            temp[0]=(1,3);
-            temp[1]=(3,-4);
-            temp[2]=(-4,-5);
-            temp[3]=(-1,4);
-            temp[4]=(1,3);
+            temp[0]=(7,7);
+            temp[1]=(7,3);
+            temp[2]=(4,-3);
+            temp[3]=(5,6);
+            temp[4]=(7,7);
             DozeField field=new DozeField(temp);
-            bool result=field.PointAffiliationAlgorithm((1,-3));
+            bool result=field.IsPointInField((4,-3));
             Console.WriteLine(result);
         }
     }
